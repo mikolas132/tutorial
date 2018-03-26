46e2102e5d6e6de4ff411a066b0e754509982726
MicaelPereira <mspereira@criticalsoftware.com>
2018-03-26 15:22:39 +0100

    first commit

---
+++ src/main/java/com/criticalsoftware/Sum.java
@@ -0,0 +1,67 @@
+package com.criticalsoftware;
+
+public class Sum{
+    int num1;
+    int num2;
+    String operation;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Sum() {
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public String Result(){
+        float result;
+        if(this.operation.equals("multiplication"))
+            result = this.num1 * this.num2;
+        else if (this.operation.equals("division"))
+            result = (float) this.num1/this.num2;
+        else if (this.operation.equals("sum"))
+            result = this.num1 + this.num2;
+        else if(this.operation.equals("subtraction"))
+            result = this.num1 - this.num2;
+        else
+            return "not match to no one operation";
+
+        return String.valueOf(result);
+
+/*        switch (this.operation){
+            case "multiplication":
+                result = this.num1 * this.num2;
+                break;
+            case "division":
+                result = (float) this.num1/this.num2;
+                break;
+            case "sum":
+                result = this.num1 + this.num2;
+                break;
+            case "subtraction":
+                result = this.num1 - this.num2;
+                break;
+            default:
+                    return "not match to no one operation";
+        }*/
+    }
+}
---
+++ src/main/java/com/criticalsoftware/rest/calculator.java
@@ -0,0 +1,33 @@
+package com.criticalsoftware.rest;
+
+
+import com.criticalsoftware.Sum;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("calculator")
+
+public class calculator {
+    @POST
+    @Path("simple")
+    public String getResult(Sum sum){
+        return sum.Result();
+    }
+
+    @GET
+    @Path("param/{username}")
+    public String getParam(@PathParam("username") String userName){
+        return "username " + userName;
+    }
+
+    @GET
+    @Path("param/{username: [A-Z][a-z]*}")
+    public String getParam1(@PathParam("username") String userName){
+        return "username " + userName;
+    }
+}
+
+
